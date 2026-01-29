cdf0cb5a7661231778ea9b377e9ed175606d50c6
João Paulo <jopaulo.as8@gmail.com>
2026-01-29 14:27:11 -0300

    chore: Getting enviroments into flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import os
 
+# FLASK SETTINGS
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+api_key = os.environ.get('GOOGLE_API_KEY')
 
-
+# ROUTES
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-
+# RUN
 if __name__ == '__main__':
     app.run()
