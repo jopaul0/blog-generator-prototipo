5b9f41710f11e2a7d1c6629a15485236f5bb447b
João Paulo <jopaulo.as8@gmail.com>
2026-01-29 16:37:32 -0300

    feat: Generate function

--- app/services/gemini.py
+++ app/services/gemini.py
@@ -1,7 +1,21 @@
 import json
 import os
+import google.generativeai as genai
+from app import api_key
 
 def load_data(path):
     with open(path, 'r', encoding='utf-8') as f:
         return json.load(f)
+
+def generate_content(prompt):
+    config = load_data(os.path.join(os.path.dirname(__file__), '..', 'config', 'gemini_config.json'))
+
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel(
+        model_name=config['model_name'],
+        generation_config=config['generation_config']
+    )
+
+    response = model.generate_content(prompt)
+    return response.text
 
